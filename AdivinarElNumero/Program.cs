820d937f5306e4fa25e098ec7321d18a0b5fa459
Carolina Perez Omodeo <caro.omodeo@gmail.com>
2021-08-18 17:55:13 -0300

    adivinar numero

---
+++ AdivinarElNumero/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AdivinarElNumero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int aleatorio = new Random().Next(0,101);
+
+            Console.WriteLine("Adivine el numero entre 0 y 100");
+            Console.WriteLine("Ingrese un numero");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            while (aleatorio != num)
+            {
+                if(num < aleatorio)
+                {
+                    Console.WriteLine("Numero incorrecto, ingrese un numero mayor");
+                    num = Convert.ToInt32(Console.ReadLine());
+                }
+                else
+                {
+                    Console.WriteLine("Numero incorrecto, ingrese un numero menor");
+                    num = Convert.ToInt32(Console.ReadLine());
+                }
+
+            }
+
+            Console.WriteLine("Felicidades, adivinaste el numero!!!!");
+            Console.ReadKey();
+        }
+    }
+}
